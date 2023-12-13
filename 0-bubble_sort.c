7f28a133972d7090828e059f571f25a8d666a237
hany0ageeb <hany0ageeb@gmail.com>
2023-12-13 13:14:58 -0800

    Task 0.Bubble sort

---
+++ 0-bubble_sort.c
@@ -0,0 +1,38 @@
+#include "sort.h"
+
+/**
+ * swap - swap values of array at i, j indeces
+ * @array: array of ints
+ * @i: first element index
+ * @j: second element index
+ */
+static void swap(int *array, int i, int j)
+{
+	int temp;
+
+	temp = array[i];
+	array[i] = array[j];
+	array[j] = temp;
+}
+/**
+ * bubble_sort -  sorts an array of integers
+ * in ascending order using the Bubble sort algorithm
+ * @array: an array of length size to sort
+ * @size: length of array
+ */
+void bubble_sort(int *array, size_t size)
+{
+	size_t i, j;
+
+	for (i = 0; i < size; ++i)
+	{
+		for (j = 0; j < size - i - 1; ++j)
+		{
+			if (array[j] > array[j + 1])
+			{
+				swap(array, j, j + 1);
+				print_array(array, size);
+			}
+		}
+	}
+}
