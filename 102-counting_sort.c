5c64a33ca7e4d4c35e2f6428941e90fc4722602a
hany0ageeb <hany0ageeb@gmail.com>
2023-12-15 09:44:06 -0800

    Task 6. Counting sort

---
+++ 102-counting_sort.c
@@ -0,0 +1,69 @@
+#include "sort.h"
+#include <stdlib.h>
+/**
+ * max - find max element if array
+ * @array: array of integers
+ * @size: array size
+ * Return: maximum element in array
+ */
+int max(int *array, size_t size)
+{
+	int val = array[0];
+	size_t i;
+
+	for (i = 1; i < size; ++i)
+		if (val < array[i])
+			val = array[i];
+	return (val);
+}
+/**
+ * init_array - initialize array element to value
+ * @array: array
+ * @size: size
+ * @value: init value
+ */
+void init_array(int *array, size_t size, int value)
+{
+	size_t i;
+
+	for (i = 0; i < size; ++i)
+		array[i] = value;
+}
+/**
+ * counting_sort - sorts an array of integers in ascending order
+ * using the Counting sort algorithm
+ * @array: integer array
+ * @size: array size
+ */
+void counting_sort(int *array, size_t size)
+{
+	int k = max(array, size), j;
+	int *output_array = NULL;
+	int *count_array = malloc(sizeof(int) * (k + 1));
+	size_t i;
+
+	if (count_array == NULL)
+		return;
+	output_array = malloc(sizeof(int) * size);
+	if (output_array == NULL)
+	{
+		if (count_array != NULL)
+			free(count_array);
+		return;
+	}
+	init_array(count_array, k + 1, 0);
+	for (i = 0; i < size; ++i)
+		count_array[array[i]]++;
+	for (j = 1; j <= k; ++j)
+		count_array[j] += count_array[j - 1];
+	print_array(count_array, k + 1);
+	for (j = (long int)size - 1; j >= 0; --j)
+	{
+		output_array[count_array[array[j]] - 1] = array[j];
+		count_array[array[j]]--;
+	}
+	for (i = 0; i < size; ++i)
+		array[i] = output_array[i];
+	free(output_array);
+	free(count_array);
+}
